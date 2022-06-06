6a50e6826d9a8bf7c6da04618fe2575c473cb007
boyin0x <94027312+boyin0x@users.noreply.github.com>
2022-06-06 04:48:03 -0300

    upgrade app to use hopr standard (#3)
    * upgrade app to use hopr standard
    
    * Add gitpod support
    
    * Update readme
    
    * Add demo video
    
    * Update README.md

--- src/WebSocketHandler.tsx
+++ src/WebSocketHandler.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import useWebsocket from "./useWebSocket";
-
+import { decode } from 'rlp'
 export const WebSocketHandler: React.FC<{
   wsEndpoint: string;
   securityToken: string;
@@ -8,14 +8,21 @@ export const WebSocketHandler: React.FC<{
   const [message, setMessage] = useState("");
   const websocket = useWebsocket({ wsEndpoint, securityToken });
   const { socketRef } = websocket;
+
+  const decodeMessage =  (msg:string): string => {
+    let uint8Array = new Uint8Array(JSON.parse(`[${msg}]`));    
+    let decodedArray = decode(uint8Array)
+    if (decodedArray[0] instanceof Uint8Array) {
+      return new TextDecoder().decode(decodedArray[0])
+    }
+    throw Error(`Could not decode received message: ${msg}`)
+  }
+
   const handleReceivedMessage = async (ev: MessageEvent<string>) => {
     try {
-      // we are only interested in messages, not all the other events coming in on the socket
-      const data = JSON.parse(ev.data);
-      console.log("WebSocket Data", data);
-      if (data.type === "message") {
-        setMessage(data.msg);
-      }
+      const data = decodeMessage(ev.data)
+      console.log("WebSocket Data: ", data);
+      setMessage(data);
     } catch (err) {
       console.error(err);
     }
